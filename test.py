27030544788019c50a7f1be2e5472548a3fec88b
Marcel <marcelsoaresribeiro@gmail.com>
2017-09-30 02:04:55 -0300

    Added test for MovingAverageStreamer

--- test.py
+++ test.py
@@ -2,11 +2,32 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from MovingAverageStreamer import MovingAverageStreamer
+
 class Test:
-    def sampleData(self):
-        df =  pd.read_csv('/home/marcel/TG/individual_stocks_5yr/AAPL_data.csv')
-        df["Close"].plot()
+    def __init__(self):
+        self.dataFrame =  pd.read_csv('/home/marcel/TG/individual_stocks_5yr/AAPL_data.csv')
+
+    def testSampleData(self):
+        self.dataFrame["Close"].plot()
         plt.show()
 
+
+    def testMovingAverageStremmer(self):
+        dataArray = self.dataFrame["Close"]
+
+        movingAverageStreamer = MovingAverageStreamer(20)
+        movingAverageStreamer.setup(dataArray[:20])
+
+        movingAverageArray = [movingAverageStreamer.lastValue]
+        for data in dataArray[20:]:
+            movingAverageArray.append(movingAverageStreamer.onData(data))
+
+        plt.plot(range(len(dataArray)-19), dataArray[19:])
+        plt.plot(movingAverageArray, 'r')
+        plt.show()
+
+
 test = Test()
-test.sampleData()
+#test.testSampleData()
+test.testMovingAverageStremmer()
